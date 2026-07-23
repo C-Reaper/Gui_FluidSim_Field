804f587ac009e7f58e111482329e5187991738fe
codeleaded <gnu.c.reaper@gmail.com>
2026-07-24 01:39:59 +0200

    update

--- src/Main.c
+++ src/Main.c
@@ -1,4 +1,4 @@
-#include "/home/codeleaded/System/Static/Library/WindowEngine1.0.h"
+#include "/home/codeleaded/System/Static/Library/WindowEngine.h"
 #include "/home/codeleaded/System/Static/Library/FluidField.h"
 
 #define FLUID_INTERACTION_RADIUS 10.0f
